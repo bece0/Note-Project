0b056c07ad9da4c1104de251c3e3e4babcc11c53
bece0 <bgmclb45@gmail.com>
2020-03-07 23:11:25 +0300

    API key kullanarak course servisinde get_active_courses isimli metodun kullanılması sağlandı

---
+++ services/_api_key_kontrol.php
@@ -0,0 +1,49 @@
+<?php
+
+header('Content-type: application/json');
+
+include '../database/database.php';
+$baglanti = BAGLANTI_GETIR();
+
+$headers = getallheaders();  
+
+$statusCode = 0;
+
+$KULLANICI_ID = NULL;
+$KULLANICI = NULL;
+$GIRIS_YAPAN_OGRENCI_MI = FALSE;
+$GIRIS_YAPAN_OGRETMEN_MI = FALSE;
+$API_ISTEGI = FALSE;
+
+if(isset($headers['x-api-key'])){
+    $API_KEY = $headers['x-api-key'];
+    $API_ISTEGI = TRUE;
+
+    $KULLANICI = KullaniciBilgileriniGetirByAPI($API_KEY);
+    if($KULLANICI != NULL){
+        $KULLANICI_ID = $KULLANICI["id"];
+    }else{
+        $statusCode = 401;
+        throw new Exception("x-api-key değeri geçersiz!");
+    }
+}else{
+    session_start();
+    //kullanici oturumu açık değil ise bu servise gelen istekeler işlenmez.
+    if(!isset($_SESSION["kullanici_id"])){
+        throw new Exception("oturum geçersiz!");
+    }
+
+    $KULLANICI_ID = $_SESSION["kullanici_id"];
+    $KULLANICI = KullaniciBilgileriniGetirById($KULLANICI_ID); 
+}
+
+if($KULLANICI != NULL){
+    if($KULLANICI["admin"] == 1){
+        $GIRIS_YAPAN_OGRETMEN_MI = TRUE;
+    }
+    if($KULLANICI["admin"] == 0){
+        $GIRIS_YAPAN_OGRENCI_MI = TRUE;
+    }
+}
+
+?>
--- services/course.php
+++ services/course.php
@@ -1,65 +1,44 @@
-<?php 
-session_start();
-header('Content-type: application/json');
+<?php
 
-//kullanici oturumu açık değil ise bu servise gelen istekeler işlenmez.
-if(!isset($_SESSION["kullanici_id"])){
-    die();
-}
-
-if(!isset($_GET["method"]) || $_GET["method"] == ""){
-    echo "method parametresi eksik!";
-    die();
-}
-
-$METHOD = $_GET["method"];
-$KULLANICI_ID = $_SESSION["kullanici_id"];
-
-// if($comment_id == NULL){
-//     echo "$comment_id yoq";
-//     die();
-// }
-
-include '../database/database.php';
-$baglanti = BAGLANTI_GETIR();
-
-$sonucObjesi = new stdClass();;
+$sonucObjesi = new stdClass();
 $sonucObjesi->sonuc = false;
 $sonucObjesi->mesaj = "";
-
-//isteği yapan kullanıcı
-$KULLANICI = KullaniciBilgileriniGetirById($KULLANICI_ID); 
-$COURSE = NULL;
-$COURSE_ID = NULL;
-
-$GIRIS_YAPAN_DERSIN_HOCASI_MI = FALSE;
-$GIRIS_YAPAN_DERSIN_ASISTANI_MI = FALSE;
-
-$statusCode = 0;
+$sonucObjesi->data = new stdClass();
 
 try{
+    include '_api_key_kontrol.php';
 
-    if(isset($_GET["ders_id"]) && $_GET["ders_id"] != ""){
-        $COURSE_ID = mysqli_real_escape_string($baglanti, $_GET["ders_id"]);
+    if(!isset($_GET["method"]) || $_GET["method"] == ""){
+        echo "method parametresi eksik!";
+        die();
     }
+    
+    $METHOD = $_GET["method"];
+    
+    $COURSE = NULL;
+    $COURSE_ID = NULL;
+    
+    $GIRIS_YAPAN_DERSIN_HOCASI_MI = FALSE;
+    $GIRIS_YAPAN_DERSIN_ASISTANI_MI = FALSE;
 
-    if(isset($_POST["ders_id"]) && $_POST["ders_id"] != ""){
-        $COURSE_ID = mysqli_real_escape_string($baglanti, $_POST["ders_id"]);
+    if($METHOD == "finish" || $METHOD == "ayril" || $METHOD == "update_image"){
+        if(isset($_GET["ders_id"]) && $_GET["ders_id"] != ""){
+            $COURSE_ID = mysqli_real_escape_string($baglanti, $_GET["ders_id"]);
+        }
+
+        if($COURSE_ID == NULL){
+            $statusCode = 400;
+            throw new Exception("ders_id parametresi eksik!");
+        }
+
+        $COURSE = DersBilgileriniGetir($COURSE_ID);
+        if($COURSE == NULL){
+            $statusCode = 404;
+            throw new Exception("Böyle bir ders bulunmuyor!!");
+        }
+
+        $GIRIS_YAPAN_DERSIN_HOCASI_MI = ($COURSE["duzenleyen_id"] == $KULLANICI_ID);
     }
-
-    if($COURSE_ID == NULL){
-        $statusCode = 400;
-        throw new Exception("ders_id parametresi eksik!");
-    }
-
-    $COURSE = DersBilgileriniGetir($COURSE_ID);
-
-    if($COURSE == NULL){
-        $statusCode = 404;
-        throw new Exception("Böyle bir ders bulunmuyor!!");
-    }
-
-    $GIRIS_YAPAN_DERSIN_HOCASI_MI = ($COURSE["duzenleyen_id"] == $KULLANICI_ID);
 
     if($METHOD == "finish"){
         
@@ -90,21 +69,31 @@ try{
         include '../includes/ortak.php';
 
         DosyaUpload("../files/images/event/", "", $COURSE["kodu"], ["png", "jpg", "jpeg"]);
+    }else if($METHOD == "get_active_courses"){
+        if($GIRIS_YAPAN_OGRETMEN_MI){
+            $sonucObjesi->data->dersler = DuzenledigiAktifDersleriGetir($KULLANICI_ID);
+            $sonucObjesi->data->asistan_dersler = AsistanOlunanDersleriGetir($KULLANICI_ID);
+        }else if($GIRIS_YAPAN_OGRENCI_MI){
+            $sonucObjesi->data->dersler = OgrencininAktifDersleriniGetir($KULLANICI_ID);
+        }
     }else{
         $statusCode = 400;
         throw new Exception("Desteklenmeyen metod : $METHOD");
     }
 
 }catch(Throwable $exp){
-    if($statusCode == 0)
+    if($statusCode == 0){
         $statusCode = 500;
+    }
 
     http_response_code($statusCode);
 
     $sonucObjesi->code = $statusCode;
     $sonucObjesi->hata = $exp->getMessage();
     $sonucObjesi->mesaj = $exp->getMessage();
-    $sonucObjesi->detay = $exp->getTraceAsString();
+
+    if($statusCode != 401)
+        $sonucObjesi->detay = $exp->getTraceAsString();
 }
 
         
