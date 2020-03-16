9f3ac044097fa9691518b883c73483e5ea03cd56
bece0 <bgmclb45@gmail.com>
2020-03-16 11:04:28 +0300

    unattend(dersten kayıt silme) metodu desteklendi

--- services/attend.php
+++ services/attend.php
@@ -7,51 +7,96 @@
  $sonucObjesi->data = new stdClass();
  
  try{
-     include '_api_key_kontrol.php';
+    include '_api_key_kontrol.php';
 
-    $COURSE_CODE = mysqli_real_escape_string($baglanti, $_GET["code"]);
- 
-    if(!isset($COURSE_CODE)){
+    if(!isset($_GET["method"]) || $_GET["method"] == "" ){
         $statusCode = 400;
-        throw new Exception("code parametresi eksik!");
+        throw new Exception("method parametresi eksik!");
     }
 
-    $COURSE = DersDetayGetir_Kod($COURSE_CODE);
+    $METOD = $_GET["method"];
 
-    if($COURSE == NULL){
-        $statusCode = 404;
-        throw new Exception("Hatalı ders kodu girildi ".$COURSE_CODE);
-    }
+    if($METOD == "attend"){
+        $COURSE_CODE = mysqli_real_escape_string($baglanti, $_GET["code"]);
+        if(!isset($COURSE_CODE)){
+            $statusCode = 400;
+            throw new Exception("code parametresi eksik!");
+        }
+
+        $COURSE = DersDetayGetir_Kod($COURSE_CODE);
+
+        if($COURSE == NULL){
+            $statusCode = 404;
+            throw new Exception("Hatalı ders kodu girildi ".$COURSE_CODE);
+        }
+
+        $COURSE_ID = $COURSE["id"];
+
+        if($COURSE["duzenleyen_id"] == $KULLANICI_ID){
+            $statusCode = 400;
+            throw new Exception("Dersin öğretmeni derse kaydolamaz!");
+        }
+    
+        $COURSE_ALREADY_REGISTERED = DerseKayitliMi($KULLANICI_ID, $COURSE_ID);
+    
+        if($COURSE_ALREADY_REGISTERED  == TRUE){
+            $statusCode = 400;
+            throw new Exception("Derse zaten kayıtlısınız.");
+        }
         
-    $COURSE_ID = $COURSE["id"];
-
-    if($COURSE["duzenleyen_id"] == $KULLANICI_ID){
-        $statusCode = 400;
-        throw new Exception("Dersin öğretmeni derse kaydolamaz!");
-    }
-
-    $COURSE_ALREADY_REGISTERED = DerseKayitliMi($KULLANICI_ID, $COURSE_ID);
-
-    if($COURSE_ALREADY_REGISTERED  == TRUE){
-        $statusCode = 400;
-        throw new Exception("Derse zaten kayıtlısınız.");
-    }
+        $kayitli = DerseKayitliOgrenciSayisi($COURSE_ID);
+        if($kayitli >= $COURSE["kontenjan"]){
+            $statusCode = 400;
+            throw new Exception("Ders kontenjanı dolu.");
+        }
     
-    $kayitli = DerseKayitliOgrenciSayisi($COURSE_ID);
-    if($kayitli >= $COURSE["kontenjan"]){
-        $statusCode = 400;
-        throw new Exception("Ders kontenjanı dolu.");
-    }
+        if(DerseKayitOl($KULLANICI_ID,  $COURSE_ID) === TRUE){
+            $sonucObjesi->sonuc = true;
+            $sonucObjesi->mesaj = "Derse kayıt olundu.";
+            LogYaz_DersKayit($KULLANICI_ID, $COURSE_ID);
+        }else{
+            $statusCode = 500;
+            throw new Exception("Derse kayıt olunamadı.");
+        }
+    }else if($METOD == "unattend"){
+        $COURSE_ID = mysqli_real_escape_string($baglanti, $_GET["courseId"]);
+        if(!isset($COURSE_ID)){
+            $statusCode = 400;
+            throw new Exception("courseId parametresi eksik!");
+        }
 
-    if(DerseKayitOl($KULLANICI_ID,  $COURSE_ID) === TRUE){
-        $sonucObjesi->sonuc = true;
-        $sonucObjesi->mesaj = "Kayıt olundu.";
-        LogYaz_DersKayit($KULLANICI_ID, $COURSE_ID);
+        $COURSE = DersDetayGetir($COURSE_ID);
+        if($COURSE == NULL){
+            $statusCode = 404;
+            throw new Exception("Hatalı ders kodu girildi ".$COURSE_CODE);
+        }
+
+        $COURSE_ID = $COURSE["id"];
+
+        if($COURSE["duzenleyen_id"] == $KULLANICI_ID){
+            $statusCode = 400;
+            throw new Exception("Dersin öğretmeni dersten ayrılamaz!");
+        }
+    
+        $COURSE_ALREADY_REGISTERED = DerseKayitliMi($KULLANICI_ID, $COURSE_ID);
+    
+        if($COURSE_ALREADY_REGISTERED  == FALSE){
+            $statusCode = 400;
+            throw new Exception("Derse zaten kayıtlı değilsiniz.");
+        }
+
+        if(DerstenKayitSil($COURSE_ID, $KULLANICI_ID) === TRUE){
+            $sonucObjesi->sonuc = true;
+            $sonucObjesi->mesaj = "Dersten kayıt silindi.";
+            LogYaz_DersKayitIptal($KULLANICI_ID, $COURSE_ID);
+        }else{
+            $statusCode = 500;
+            throw new Exception("Dersten kayıt silinemedi.");
+        }
     }else{
-        $statusCode = 500;
-        throw new Exception("Derse kayıt olunamadı.");
+        $statusCode = 400;
+        throw new Exception("Bilinmeyen method parametresi : $METOD");
     }
-    
     
 }catch(Throwable $exp){
     if($statusCode == 0){
@@ -60,6 +105,7 @@
 
     http_response_code($statusCode);
 
+    $sonucObjesi->sonuc = false;
     $sonucObjesi->code = $statusCode;
     $sonucObjesi->hata = $exp->getMessage();
     $sonucObjesi->mesaj = $exp->getMessage();
